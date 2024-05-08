389130c9c24d2385998080cf73e54557f5f130e9
mukesh <mukesh_subudhi@comcast.com>
2024-05-08 13:09:23 +0530

    First Commit

---
+++ cypress/e2e/GoogleDoc.js
@@ -0,0 +1,44 @@
+export class GoogleDoc {
+
+    enter_name = '.whsOnd'
+    select_age = '#i12 > .vd3tt > .AB7Lab'
+    dropdown = '.KKjvXb'
+    select_yes = "div[data-value='Yes'][role='option']"
+    submit_form = '.lRwqcd > .uArJ5e > .l4V7wb'
+    confirm_msg='.vHW8K'
+    error_msg='.RHiWt'
+    clear_form='.nYdzXd > .uArJ5e > .l4V7wb > .NPEfkd'
+
+    enterName(name) {
+        cy.get(this.enter_name).type(name)
+    }
+
+    selectAge() {
+        cy.get(this.select_age).click()
+    }
+
+    selectExercise() {
+        cy.get(this.dropdown).click()
+        cy.wait(2000)
+        cy.get(this.select_yes).click()
+    }
+
+    submitForm() {
+        cy.get(this.submit_form).click()
+    }
+
+    verifySubmit() {
+        cy.get(this.confirm_msg).should('contain', 'Your response has been recorded.')
+        cy.url().should('contain', 'formResponse')
+    }
+
+    verifyErrorMsg() {
+        cy.get(this.error_msg).should('contain', 'This is a required question')
+    }
+
+    clearForm() {
+        cy.get(this.clear_form).click()
+        cy.get('.OE6hId > [data-id="EBS5u"] > .l4V7wb > .NPEfkd').click()
+        cy.wait(10000)
+    }
+}
---
+++ cypress/e2e/form_submission.cy.js
@@ -0,0 +1,45 @@
+import { GoogleDoc } from "./GoogleDoc"
+
+const googledoc = new GoogleDoc()
+describe('Submit the Google Form', () => {
+    var name = 'Test'
+    beforeEach(() => {
+        cy.visit('https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform')
+    })
+
+    it('Submit Form', () => {
+        googledoc.enterName(name);
+        googledoc.selectAge();
+        googledoc.selectExercise();
+        googledoc.submitForm();
+        googledoc.verifySubmit();
+    })
+
+    it('Submit Form Without name', () => {
+        googledoc.selectAge();
+        googledoc.selectExercise()
+        googledoc.submitForm()
+        googledoc.verifySubmit();
+    })
+
+    it('Submit Form Without age', () => {
+        googledoc.enterName(name)
+        googledoc.selectExercise()
+        googledoc.submitForm()
+        googledoc.verifyErrorMsg()
+    })
+
+    it('Submit Form Without exercise', () => {
+        googledoc.enterName(name)
+        googledoc.selectAge()
+        googledoc.submitForm()
+        googledoc.verifyErrorMsg()
+    })
+
+    it('Clear Form', () => {
+        googledoc.enterName(name)
+        cy.get('.whsOnd').should('have.value', name)
+        googledoc.clearForm()
+        cy.get('.whsOnd').should('have.value', '')
+    })
+})
